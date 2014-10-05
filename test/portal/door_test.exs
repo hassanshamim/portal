3ba0e6e6193bd55a7ba5a5cd410ccbf58a9a04fc
Hassan Shamim <hassan.has.you@gmail.com>
2014-10-05 18:19:34 -0500

    Test Portal.Door

--- test/portal/door_test.exs
+++ test/portal/door_test.exs
@@ -1,8 +1,44 @@
 defmodule PortalDoorTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+
+  setup do
+    {:ok, door} = Portal.Door.start_link(:orange)
+    {:ok,
+      [door: door, color: :orange]
+    }
+  end
 
   test "Create a Door by atom" do
-    assert {:ok, door} = Portal.Door.start_link(:orange) 
+    {:ok, door} = Portal.Door.start_link(:purple)
     assert is_pid(door)
+  end
+
+  test "Door can be retrieved by pid or atom", %{door: door, color: color} do
+    assert Portal.Door.get(door)
+    assert Portal.Door.get(color)
+  end
+
+  test "Nonexistant Door cannot be retrieved" do
+    assert catch_exit Portal.Door.get(:NonExistant)
+  end
+
+  test "Door starts with empty list", %{color: color} do
+    assert [] == Portal.Door.get(color)
+  end
+
+  test "Door acts as stack", %{door: door, color: color} do
+    for n <- [1, 2, 3, 4], do: Portal.Door.push(door, n)
+
+    assert [4, 3, 2, 1] = Portal.Door.get(color)
+  end
+
+  test "Door can remove last added values", %{door: door} do
+    for n <- [1, 2, 3, 4], do: Portal.Door.push(door, n)
+
+    assert {:ok, 4} = Portal.Door.pop(door)
+  end
+
+  test "Can't pop an empty door", %{door: door} do
+    assert :error = Portal.Door.pop(door)
   end
 end
